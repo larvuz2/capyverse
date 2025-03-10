dbfc46661a0f6884a213b53bc15c05a6ff0f604f
larvuz2 <155197431+larvuz2@users.noreply.github.com>
2025-03-09 22:05:20 -0700

    Simplify Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
-import wasm from 'vite-plugin-wasm';
-import topLevelAwait from 'vite-plugin-top-level-await';
-
 export default {
   base: './',
   build: {
     outDir: 'dist'
   },
   publicDir: 'public',
-  root: './',
-  plugins: [
-    wasm(),
-    topLevelAwait()
-  ],
-  optimizeDeps: {
-    exclude: ['@dimforge/rapier3d']
-  }
-} 
+  root: './'
+}
